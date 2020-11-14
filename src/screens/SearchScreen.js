5100b276c1ba32c16476fb0c4e2fcf5e59f91f26
Ali Sajid <alisajid552@yahoo.com>
2020-11-13 23:42:52 -0800

    add search bar functionality

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 
+import SearchBar from "../components/SearchBar";
+
 const SearchScreen = () => {
+  const [term, setTerm] = useState("");
+
   return (
-    <View>
-      <Text>Search Screen</Text>
+    <View style={styles.containerStyle}>
+      <SearchBar
+        term={term}
+        onTermChange={(newTerm) => setTerm(newTerm)}
+        onTermSubmit={() => console.log("term submitted")}
+      />
     </View>
   );
 };
 
-const Styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  containerStyle: {
+    flex: 1,
+    backgroundColor: "#ffffff",
+  },
+});
 
 export default SearchScreen;
