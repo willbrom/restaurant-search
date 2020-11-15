85d4d810727dc9a3d6f0d3c66ce08aa1f15b4aba
Ali Sajid <alisajid552@yahoo.com>
2020-11-14 16:10:27 -0800

    create yelp get request

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -3,16 +3,27 @@ import { View, Text, StyleSheet } from "react-native";
 
 import SearchBar from "../components/SearchBar";
 
+import yelp from "../api/yelp";
+
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
+  const [results, setResults] = useState([]);
+
+  const searchApi = async () => {
+    const response = await yelp.get("/search", {
+      params: {
+        limit: 50,
+        term,
+        location: "san jose",
+      },
+    });
+    setResults(response.data.businesses);
+  };
 
   return (
     <View style={styles.containerStyle}>
-      <SearchBar
-        term={term}
-        onTermChange={(newTerm) => setTerm(newTerm)}
-        onTermSubmit={() => console.log("term submitted")}
-      />
+      <SearchBar term={term} onTermChange={setTerm} onTermSubmit={searchApi} />
+      <Text>search term results found: {results.length}</Text>
     </View>
   );
 };
