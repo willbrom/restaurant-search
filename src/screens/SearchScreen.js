0b40681e333af6d2183cced96ec34be443048e2c
Ali Sajid <alisajid552@yahoo.com>
2020-11-14 22:41:59 -0800

    add useResult hook for yelp search

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,34 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
-
 import SearchBar from "../components/SearchBar";
-
-import yelp from "../api/yelp";
+import useResults from "../hooks/useResults";
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
-  const [results, setResults] = useState([]);
-  const [errorMessage, setErrorMessage] = useState("");
-
-  const searchApi = async (searchTerm) => {
-    console.log("Hello there");
-    try {
-      const response = await yelp.get("/search", {
-        params: {
-          limit: 50,
-          term: searchTerm,
-          location: "san jose",
-        },
-      });
-      setResults(response.data.businesses);
-    } catch (e) {
-      setErrorMessage("Some thing went wrong.");
-    }
-  };
-
-  useEffect(() => {
-    searchApi("pizza");
-  }, []);
+  const [results, errorMessage, searchApi] = useResults();
 
   return (
     <View style={styles.containerStyle}>
